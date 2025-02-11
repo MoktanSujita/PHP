b6c52e7bdc75c3ea793eb473d12aa4b90ac416fe
Moktan Sujita <sujitamoktan77@gmail.com>
2025-02-11 21:19:17 +0545

    commit

---
+++ assi_op.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $a=60;
+    $b=8;
+    $a+=$b;
+    echo"New A=$a";
+    $a-=$b;
+    echo"<br>Initial A=$a";
+    $a*=$b;
+    echo"<br>Multiple of A and B=$a";
+    $a/=$b;
+    echo"<br>480/8=$a";
+    $a%=$b;
+    echo"<br>60%8=$a";
+
+    ?>
+</body>
+</html>
