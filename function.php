967216596e747c29779561afc27d79c14067f71c
Moktan Sujita <sujitamoktan77@gmail.com>
2025-02-11 21:21:23 +0545

    commit

---
+++ function.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+      function name($a) : void {
+          echo "My name is $a";
+          echo"<br>";
+      }
+      name("Sujita");
+      name("Moktan");
+?>
+</body>
+</html>
