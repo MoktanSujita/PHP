4f981b45c75fba5ed2f83550a43086f6136be064
Moktan Sujita <sujitamoktan77@gmail.com>
2025-04-17 22:10:13 +0545

    Session variable data retrival

--- session/sessionData_retrival.php
+++ session/sessionData_retrival.php
@@ -2,7 +2,7 @@
      session_start();
     if(isset($_SESSION['username']))     //isset 
     {
-        echo "Welcome," .$_SESSION['username'];  //rerives and concatinate username
+        echo "Welcome," .$_SESSION['username'];     //rerives and concatinate username
     }
     else
     {
