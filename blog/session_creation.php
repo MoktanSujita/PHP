d9b9ffd7800cf17679c4135ea380474c483e6cbb
Moktan Sujita <sujitamoktan77@gmail.com>
2025-04-17 21:46:51 +0545

    Session data creation and storage

---
+++ blog/session_creation.php
@@ -0,0 +1,10 @@
+<?php
+    session_start();//starting a session
+
+    $_SESSION['username']="Jonney Jonney";   //data for session
+    $_SESSION['email']="jonneyjonney@gmail.com";
+
+    echo "session data stored";
+
+    session_destroy();        //once session is started, it needs to be destroyed manually
+?>
