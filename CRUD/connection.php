c36cfd999dbe3246f458942295070b0f5d0b49ed
Moktan Sujita <sujitamoktan77@gmail.com>
2025-04-19 22:12:53 +0545

    Establishing connection with database

--- CRUD/connection.php
+++ CRUD/connection.php
@@ -1,5 +1,5 @@
 <?php
-    mysqli_connect("localhost","root","","test_db");
+    $conn=mysqli_connect("localhost","root","","test_db");
     if(!$conn)
     {
         die ("Connection failed:".mysqli_connect_error());
